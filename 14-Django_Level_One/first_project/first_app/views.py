08c28e6610b3041a1d111c33b5bd22e9c1298067
Jason Eisele <jmeisele@yahoo.com>
2018-11-07 12:18:01 -0600

    Updated templates directory

--- 14-Django_Level_One/first_project/first_app/views.py
+++ 14-Django_Level_One/first_project/first_app/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 #     return HttpResponse("Hello World!")
 
 def index(request):
-    my_dict = {'insert_me':"Hello I am from views.py!"}
+    my_dict = {'insert_me':"Now I am coming from first_app/index.html "}
     return render(request,'first_app/index.html',context=my_dict)
